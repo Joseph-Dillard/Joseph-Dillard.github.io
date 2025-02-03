0b9ccf93a64ab3f5e09fae4778aba521497dacb3
Joseph-Dillard <greenjodillard@gmail.com>
2025-02-03 14:50:44 -0600

    Rough Draft 1

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+        document.querySelector(".nav-bar").classList.add("show-nav");
+    }, 3000); // Matches the name animation duration
+});
